9cdb345194625a8f3201a466dd7598ed0fae287c
THUMILA <thumilakasthuricamp525@gmail.com>
2025-10-20 17:56:10 +0530

    Global arrays added

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
+#define MAX_CITIES 30
+#define MAX_DELIVERIES 50
+#define MAX_NAME 50
+#define FUEL_PRICE 310.0
+
+// Global arrays
+const int vehicleCapacity[] = {1000, 5000, 10000};
+const int vehicleRate[] = {30, 40, 80};
+const int vehicleSpeed[] = {60, 50, 45};
+const int vehicleEfficiency[] = {12, 6, 4};
+const char vehicleNames[][10] = {"Van", "Truck", "Lorry"};
