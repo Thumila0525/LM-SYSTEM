2fd77a5ed1c7f73a4e53540501c8b6c46c60f97e
THUMILA <thumilakasthuricamp525@gmail.com>
2025-10-20 17:58:30 +0530

    Function declaration

--- main.c
+++ main.c
@@ -14,3 +14,16 @@ const int vehicleRate[] = {30, 40, 80};
 const int vehicleSpeed[] = {60, 50, 45};
 const int vehicleEfficiency[] = {12, 6, 4};
 const char vehicleNames[][10] = {"Van", "Truck", "Lorry"};
+
+// Function declarations
+void displayMenu();
+int addCity(char cities[][MAX_NAME], int cityCount);
+int manageCities(char cities[][MAX_NAME], int cityCount);
+void manageDistances(char cities[][MAX_NAME], int cityCount, int distance[][MAX_CITIES]);
+void displayDistanceTable(char cities[][MAX_NAME], int cityCount, int distance[][MAX_CITIES]);
+int handleDelivery(char cities[][MAX_NAME], int cityCount, int distance[][MAX_CITIES],int deliveryCount, float deliveries[][9]);
+void displayDeliveryEstimate(char cities[][MAX_NAME], int source, int dest, int dist,int weight, int vehicleType);
+void findShortestPath(int distance[][MAX_CITIES], int cityCount, int source, int dest,int visited[], int currentDist, int minDist[], int currentPath[],int pathLen, int bestPath[], int bestPathLen[]);
+void showReports(int deliveryCount, float deliveries[][9], char cities[][MAX_NAME]);
+void saveToFile(char cities[][MAX_NAME], int cityCount, int distance[][MAX_CITIES],int deliveryCount, float deliveries[][9]);
+int loadFromFile(char cities[][MAX_NAME], int cityCount, int distance[][MAX_CITIES],int deliveryCount, float deliveries[][9]);
